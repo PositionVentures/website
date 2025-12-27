8b67bd6f8b5608f46110016c4935ea68b3ed4947
Tony Dong <tonydong91@gmail.com>
2025-12-26 21:24:33 -0800

    Add Form20-2.pdf

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com', 'cdn.prod.website-files.com', 'ubiquity6.com', 'www.persona-ai.ai'],
   },
+  async headers() {
+    return [
+      {
+        source: '/Form20-2.pdf',
+        headers: [
+          { key: 'X-Robots-Tag', value: 'noindex, nofollow, noarchive' },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
